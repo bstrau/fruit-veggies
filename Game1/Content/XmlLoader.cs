5e3ca7343babd8a4b26149f87f9766abc7b459be
Benjamin Strauß <benjamin-strauss@t-online.de>
2016-06-02 21:08:17 +0200

    lol

--- Game1/Content/XmlLoader.cs
+++ Game1/Content/XmlLoader.cs
@@ -38,8 +38,7 @@ namespace Game1.Content
 
                     case "RESSOURCE":
                         Tile tile = new RessourcesTile(Tile.TileType.RESSOURCE, node);
-                        // Tile wird eigentlich vom Konstruktor registriert  
-                        //Tile.Tiles.Add(tile.GetId(), tile);
+
                         break;
 
                     case "TREASURE":
@@ -50,34 +49,5 @@ namespace Game1.Content
             }
         }
 
-        public static Dictionary<String, String> loadAllUnits(String xmlfilepath)
-        {
-            Dictionary<String, String> ret = new Dictionary<String, String>();
-
-            XmlDocument xdoc = new XmlDocument();
-
-            xdoc.Load(xmlfilepath);
-
-            XmlNodeList units = xdoc.GetElementsByTagName("unit");
-
-            for(int i=0; i< units.Count;i++)
-            {
-                XmlElement unit = (XmlElement)units.Item(i);
-                String id = unit.GetAttribute("id");
-                String title = units.Item(i).SelectSingleNode("/title").Value;
-                String accessible = units.Item(i).SelectSingleNode("/accessible").Value;
-                String graphic = units.Item(i).SelectSingleNode("/graphic").Value;
-                String cash_rounds = units.Item(i).SelectSingleNode("/cash/rounds").Value;
-                String cash_amount = units.Item(i).SelectSingleNode("/cash/amount").Value;
-                
-                //GraphicsObject test = new GraphicsObject(Content.Load<Texture2D>(graphic));
-                //dict.Add(id, new Tile(, 0, 0, Convert.ToBoolean(accessible));
-            }
-            return ret;
-        }
-
-        public static void LoadMap()
-        { 
-        }
     }
 }
