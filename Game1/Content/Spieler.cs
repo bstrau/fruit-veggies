3570e4be898482f7667a731614b6207926a68792
Georg Fischer <georg.fischer@bfn-it.de>
2016-06-03 09:21:23 +0200

    Klassen angelegt und neue Bilder

---
+++ Game1/Content/Spieler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game1.Content
+{
+    class Spieler
+    {
+        GraphicsObject graphics;
+        GraphicsObject RessourcesTile;
+
+        protected string title;
+        protected bool tot ;
+        protected string id;
+        //FIXME 1 Noch zu klären
+        protected int Fraktion;
+        protected int Sound;
+        //END VON FIXME 1
+
+        public String GetId()
+        {
+            return id;
+        }
+
+
+
+    }
+}
---
+++ Game1/Content/Unit.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Game1.Content
+{
+    class Unit
+    {
+        GraphicsObject graphics;
+
+        protected int xPos, yPos;
+        protected Spieler spieler;
+        protected string id;
+        protected string title;
+        protected UnitType type;
+        //FIXMEE Noch zu klären 1
+        protected string sound;
+        //Ende 1
+
+        public void SetPos(int x, int y)
+        {
+            xPos = x;
+            yPos = y;
+        }
+        public String GetId()
+        {
+            return id;
+        }
+        public enum UnitType
+        {
+            STANDARD,
+            APFEL,
+            BANANE,         
+            MAXEINHEIT
+        }
+
+        public Unit(UnitType type, XmlNode node)
+        {
+            id = node.Attributes.GetNamedItem("id").Value;
+            this.type = type;
+
+            title = node.SelectSingleNode("title").InnerText;
+
+            String graphic = node.SelectSingleNode("graphic").InnerText;
+
+            graphics = GraphicsObject.graphicObjects[graphic];
+        }
+
+        public Unit(GraphicsObject graphics, int x, int y, bool accessible)
+        {
+            this.graphics = graphics;
+            xPos = x;
+            yPos = y;
+        }
+
+        public void Draw(SpriteBatch batch)
+        {
+            graphics.SetPos(xPos, yPos);
+            graphics.Draw(batch);
+        }
+
+        // Global erreichbare Liste aller Tiles.
+        public static Dictionary<String, Unit> Units = new Dictionary<string, Unit>();
+
+    }
+}
